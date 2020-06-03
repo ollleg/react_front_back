9690ee9317cbee7b300f8b88e980138cffaacb38
Oleh <oleh.polishchuk@flamefy.com>
2020-06-03 18:37:39 +0200

    Fixed test

--- front/src/components/App.test.js
+++ front/src/components/App.test.js
@@ -2,8 +2,35 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import { routerMiddleware } from 'connected-react-router'
+import createSagaMiddleware from 'redux-saga'
+
+import createRootReducer from 'reducers'
+import 'styles/index.scss'
+import history from "utils/history"
+
+const sagaMiddleware = createSagaMiddleware()
+const composeEnhancers =
+  process.env.NODE_ENV !== 'production' &&
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose
+
+const store = createStore(
+  createRootReducer(history),
+  composeEnhancers(
+    applyMiddleware(
+      routerMiddleware(history),
+      sagaMiddleware,
+      thunkMiddleware
+    )
+  )
+)
+
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Learn React/i);
+  const { getByText } = render(<App store={store} history={history}/>);
+  const linkElement = getByText(/Add product/i);
   expect(linkElement).toBeInTheDocument();
 });
